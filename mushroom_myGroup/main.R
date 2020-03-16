f82cc79c6116de6b074d0bff18e2a730894d3255
Julien Genovese <juliengenovese@hotmail.it>
2020-03-16 20:31:17 +0100

    ciao

--- mushroom_myGroup/main.R
+++ mushroom_myGroup/main.R
@@ -1,7 +1,18 @@
 # Analyzing Mushroom data
-
+library(tidyverse)
+library(randomForest)
+library(forecast)
 
 pathinput<-"./mushrooms.csv"
 dataset<-read.csv2(pathinput , sep =",")
 head(dataset)
 str(dataset)
+dataset <- dataset %>% select(-"veil.type")
+df<- dataset %>% select(c("class","cap.shape","cap.surface","cap.color","bruises","odor"))
+
+df<-df %>% group_by(cap.shape,class) 
+summarise(df, n())
+colSums(is.na(dataset)) 
+df<- dataset %>% select(-"stalk.root")
+model<-randomForest(class ~. ,data = df, ntree= 500)
+varImpPlot(model)
